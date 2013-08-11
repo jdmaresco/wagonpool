dc432d2e7238364494ef232c7dfc44757600da8b
JD Maresco <jdmaresco@gmail.com>
2013-08-11 10:13:04 -0700

    Commit initial refactor of Rails app.

---
+++ README.md
@@ -0,0 +1,1 @@
+Calculates estimated cost breakdown for a group of people, a specific car, and the current cost of gas.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wagonpool::Application.config.secret_key_base = '5517058c7412d67485e5f61866baa884b2aff69cf28705dceee9c372fbb1d97a95a9bba7c4e1cb84e750fe404b33b812f0dc35d94d7b37d74a5ed8faa8951be8'
